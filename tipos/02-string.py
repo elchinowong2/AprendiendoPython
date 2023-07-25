6f4478ba5b3cebaf19e2330e424c97857fa43622
elchinowong2 <thewong26@gmail.com>
2023-07-24 19:45:48 -0500

    Uso de los metodos de string archivo 04-metodos-st

--- tipos/02-string.py
+++ tipos/02-string.py
@@ -3,6 +3,8 @@ descripcion_curso = """bueno probando triple comilla"""
 print(descripcion_curso)
 
 
-print(nombre_curso[1])
+nombre_completo = f"{nombre_curso[0:8]} {descripcion_curso[0:8]}"
 
+print(nombre_curso[1])
 print(nombre_curso[0:8])
+print(nombre_completo)
---
+++ tipos/04-metodos-string.py
@@ -0,0 +1,13 @@
+animal = "  Chanchito Feliz  "
+
+print(animal.upper())
+print(animal.lower())
+print(animal.strip().capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("Fe"))
+print(animal.replace("Cha", "hps"))
+print("Chan" in animal)
+print("Chan" not in animal)
