8fda34c808fab58f9abdc033b9b6d8ec69d8b8db
elchinowong2 <thewong26@gmail.com>
2023-08-03 21:36:00 -0500

    jugando con arreglos

---
+++ tipos/15-buscarenIndice.py
@@ -0,0 +1,9 @@
+mascotas = ["Pelusa", "Gato", "chancho", "pucca"]
+print(mascotas.index("Pelusa"))
+
+if "Pelusa" in mascotas:
+    print("SI existe")
+
+print(mascotas.index("Pelusa"))
+
+print(mascotas.count("Gato"))
